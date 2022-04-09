b87b92b18956c15d2e4632e443d3c39289c6aea3
ericpkatz <ericpkatz@gmail.com>
2022-04-09 10:33:54 -0400

    add oauth for github

--- db.js
+++ db.js
@@ -40,26 +40,45 @@ const GITHUB_USER_URL = 'https://api.github.com/user';
 //the authenticate methods is passed a code which has been sent by github
 //if successful it will return a token which identifies a user in this app
 User.authenticate = async(code)=> {
-  throw new Error('noooooooooo');
 
-  //consider modifying /github/callback route in app.js in order to test incrementally
-  //(STEP 1) TODO - make axios post to GITHUB_TOKEN_URL with code, client_id, client_secret;
-  /*
-   * use { headers: { accept: 'application/json' }} for easier response handling
-   */
-
-  //(STEP 2) TODO - use access_token from response to make get request to GITHUB_USER_URL
-  //send header with Authorization and value 'token whatever_access_token_is'
-  /*
-    headers: {
-      Authorization: `token ${access_token}`
+  let response = await axios.post(GITHUB_TOKEN_URL,
+    {
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      code
+    },
+    {
+      headers: { accept: 'application/json' }
     }
-  */
+  );
+  const { data } = response;
+  if(data.error){
+    const error = Error(data.error_description);
+    error.status = 401;
+    throw error;
+  }
 
-  //(STEP 3) TODO - use the login value from github to identify user in app
-  //login from github should map to username in application
-  //if the user does not exist, create the user
-  //return a JWT token which has an id property with the users id
+  const access_token = response.data.access_token;
+
+  response = await axios.get(
+    GITHUB_USER_URL,
+    {
+      headers: {
+        Authorization: `token ${access_token}`
+      }
+    }
+  );
+  const profile = response.data;
+
+  let user = await User.findOne({
+    where: { username: profile.login }
+  });
+  if(!user){
+    user = await User.create({ username: profile.login });
+  }
+
+  return jwt.sign({ id: user.id }, process.env.JWT);
+
 };
 
 const syncAndSeed = async()=> {
